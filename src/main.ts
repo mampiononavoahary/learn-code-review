6bd3cc5dd768953c4f719e825f98a6eab96180a4
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-05-17 13:37:30 +0300

    My actions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as github from '@actions/github'
+import { readFileSync } from 'fs'
 
 /**
  * The main function for the action.
@@ -8,18 +9,43 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // Get the PR number from the event payload
+    const prNumber = github.context.payload.pull_request?.number
+    if (!prNumber) {
+      // Fail the workflow run if the PR number is not found
+      core.setFailed('PR number not found')
+      return
+    }
+    // Find the required reviewers
+    const reviewers = readFileSync('REVIEWERS', 'utf-8').trim()
+    if (!reviewers) {
+      // Fail the workflow run if the reviewers are not found
+      core.setFailed('Reviewers not found in REVIEWERS file')
+      return
+    }
+    // Get all reviewers for the PR
+    const githubToken = core.getInput('github_token')
+    const octokit = github.getOctokit(githubToken)
+    const reviewersResponse = await octokit.rest.pulls.listReviews({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber
+    })
+    // Check if all required reviewers have reviewed the PR
+    let approved = false
+    reviewersResponse.data.forEach((review) => {
+      if (
+        review?.user &&
+        review.user.login === reviewers &&
+        review.state === 'APPROVED'
+      ) {
+        approved = true
+      }
+    })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if (!approved) {
+      core.setFailed('Not all required reviewers have approved the PR')
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
